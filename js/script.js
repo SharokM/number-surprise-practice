16c769f2f7dca7323da02716f15a7c84639a5663
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-02-06 19:39:21 +0000

    clear event added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var box1 = document.querySelector(".box-1");
 var box2 = document.querySelector(".box-2");
 var box3 = document.querySelector(".box-3");
+var clear = document.querySelector("clear");
 
 document.addEventListener("keydown", function (e) {
     var key = e.key;
@@ -10,5 +11,16 @@ document.addEventListener("keydown", function (e) {
         box2.innerText = "😸";
     } else if (key === "3") {
         box3.innerText = "😊";
+    }
+});
+
+document.addEventListener("click", function (e) {
+    var key = e.key;
+    if (key !== "1") {
+        box1.innerText = "1";
+    // } else if (key !== "2") {
+        box2.innerText = "2";
+    // } else if (key !== "3") {
+        box3.innerText = "3";
     }
 });
